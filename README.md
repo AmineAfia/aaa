9da15368f1d6e5120298da0d19a345a0e741cfb9
Amine Afia <amine@blocktorch.xyz>
2023-07-22 14:39:14 +0200

    feat: added minimal version (biconomy tests work now)

--- README.md
+++ README.md
@@ -1,23 +1,57 @@
 
-# Node JS + Typescript Starter 
+# Biconomy Smart Account (Smart Contract Wallet) Overview
 
-This is a basic starter kit to create Node JS scripts with Typescript. It includes only a script for development and is meant for educational puproses. You will need additional scripts to optimize for production.
+Biconomy Modular Smart Account is an [EIP-4337](https://eips.ethereum.org/EIPS/eip-4337) compatible modular smart contract wallet.
+Smart Account is ownerless by nature. UserOp and txns validation happens in Authorization Modules.
 
+Smart Account is designed in such a way that it is:
 
-## Installation
+- Modular => highly customizable and extandable. 
+- Cheap to deploy proxy copies of an implementation (user wallets)
+- Wallet addresses are counterfactual in nature (you can know the address in advance and users can have the same address across all EVM chains)
+- Deployment cost can be sponsored 
 
-Clone this repository and then run
+# How to run the project
 
-```bash
-  yarn
+This project demonstrates an advanced Hardhat use case, integrating other tools commonly used alongside Hardhat in the ecosystem.
+
+## 1. Install
+```shell
+> npm install
+// or
+> yarn install
 ```
 
-Alternatively you can use `npm` or `pnpm` if that suits you. 
+## 2. Configure
+Place a mnemonic in a `.secret` file in the root folder of the project.
 
-The main purpose of this repository is to serve as the starting point for the Biconomy SDK Quick Start guide. 
+### 3. Run
 
+The project comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts. It also comes with a variety of other tools, preconfigured to work with the project code.
 
+Try running some of the following tasks:
 
-## Pull requests welcome
+```shell
+npx hardhat test
 
-Have any improvements to be made? Feel free to make a Pull Request! 
+// other
+npx hardhat accounts
+npx hardhat compile
+npx hardhat clean
+npx hardhat node
+npx hardhat help
+REPORT_GAS=true npx hardhat test
+npx hardhat coverage
+npx hardhat run scripts/deploy.ts
+TS_NODE_FILES=true npx ts-node scripts/deploy.ts
+npx eslint '**/*.{js,ts}'
+npx eslint '**/*.{js,ts}' --fix
+npx prettier '**/*.{json,sol,md}' --check
+npx prettier '**/*.{json,sol,md}' --write
+npx solhint 'contracts/**/*.sol'
+npx solhint 'contracts/**/*.sol' --fix
+```
+
+# Performance optimizations
+
+For faster runs of your tests and scripts, consider skipping ts-node's type checking by setting the environment variable `TS_NODE_TRANSPILE_ONLY` to `1` in hardhat's environment. For more details see [the documentation](https://hardhat.org/guides/typescript.html#performance-optimizations).
--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,65 @@
-import {config} from 'dotenv';
-import {IBundler, Bundler} from '@biconomy/bundler';
-import {ChainId} from '@biconomy/core-types';
-import {BiconomySmartAccount, BiconomySmartAccountConfig, DEFAULT_ENTRYPOINT_ADDRESS} from '@biconomy/account';
-import {Wallet, providers, ethers} from 'ethers';
+import { config } from "dotenv";
+import { IBundler, Bundler } from "@biconomy/bundler";
+import { ChainId } from "@biconomy/core-types";
+import {
+  BiconomySmartAccount,
+  BiconomySmartAccountConfig,
+  DEFAULT_ENTRYPOINT_ADDRESS,
+} from "@biconomy/account";
+import { Wallet, providers, ethers } from "ethers";
 
 config();
-const provider = new providers.JsonRpcProvider('https://rpc.ankr.com/polygon_mumbai');
-const wallet = new Wallet(process.env.PRIVATE_KEY || '', provider);
+const provider = new providers.JsonRpcProvider(
+  "https://rpc.ankr.com/polygon_mumbai"
+);
+const wallet = new Wallet(process.env.PRIVATE_KEY || "", provider);
 const bundler: IBundler = new Bundler({
-	bundlerUrl: 'https://bundler.biconomy.io/api/v2/80001/abc',
-	chainId: ChainId.POLYGON_MUMBAI,
-	entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
+  bundlerUrl: "https://bundler.biconomy.io/api/v2/80001/abc",
+  chainId: ChainId.POLYGON_MUMBAI,
+  entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
 });
 
 const biconomySmartAccountConfig: BiconomySmartAccountConfig = {
-	signer: wallet,
-	chainId: ChainId.POLYGON_MUMBAI,
-	bundler: bundler,
+  signer: wallet,
+  chainId: ChainId.POLYGON_MUMBAI,
+  bundler: bundler,
 };
 
+// 1. create SAC
 async function createAccount() {
-	const biconomyAccount = new BiconomySmartAccount(biconomySmartAccountConfig);
-	const biconomySmartAccount = await biconomyAccount.init();
-	console.log('owner: ', biconomySmartAccount.owner);
-	console.log('address: ', await biconomySmartAccount.getSmartAccountAddress());
-	return biconomyAccount;
+  const biconomyAccount = new BiconomySmartAccount(biconomySmartAccountConfig);
+  const biconomySmartAccount = await biconomyAccount.init();
+  console.log("owner: ", biconomySmartAccount.owner);
+  console.log("address: ", await biconomySmartAccount.getSmartAccountAddress());
+  return biconomyAccount;
 }
+// 2. deploy key manager
+// 3. deploy validation module
+// white list Arsenii
+// Arsenii triggers a transaction through the SA
 
+
+// trigger transaction
 async function createTransaction() {
-	console.log('creating account');
+  console.log("creating account");
 
-	const smartAccount = await createAccount();
+  const smartAccount = await createAccount();
 
-	const transaction = {
-		to: '0x14a4CF64e8BdC492D7fAF782896E22C79334b1Fe',
-		data: '0x',
-		value: ethers.utils.parseEther('0.1'),
-	};
+  const transaction = {
+    to: "0x14a4CF64e8BdC492D7fAF782896E22C79334b1Fe",
+    data: "0x",
+    value: ethers.utils.parseEther("0.1"),
+  };
 
-	const userOp = await smartAccount.buildUserOp([transaction]);
-	userOp.paymasterAndData = '0x';
+  const userOp = await smartAccount.buildUserOp([transaction]);
+  userOp.paymasterAndData = "0x";
 
-	const userOpResponse = await smartAccount.sendUserOp(userOp);
+  const userOpResponse = await smartAccount.sendUserOp(userOp);
 
-	const transactionDetail = await userOpResponse.wait();
+  const transactionDetail = await userOpResponse.wait();
 
-	console.log('transaction detail below');
-	console.log(transactionDetail);
+  console.log("transaction detail below");
+  console.log(transactionDetail);
 }
 
 createTransaction();
