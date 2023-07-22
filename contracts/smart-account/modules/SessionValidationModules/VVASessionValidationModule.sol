bf6def9a217dadcfd5c4c00137a7d8feded175c3
catalyst17 <37663786+catalyst17@users.noreply.github.com>
2023-07-23 00:25:10 +0200

    feat: add Sismo Verification

--- contracts/smart-account/modules/SessionValidationModules/VVASessionValidationModule.sol
+++ contracts/smart-account/modules/SessionValidationModules/VVASessionValidationModule.sol
@@ -2,8 +2,12 @@
 pragma solidity 0.8.17;
 import "./ISessionValidationModule.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@sismo-core/sismo-connect-solidity/contracts/libs/SismoLib.sol";
 
-contract VVASessionValidationModule {
+contract VVASessionValidationModule is SismoConnect {
+    // call SismoConnect constructor with your appId
+    constructor(bytes16 appId) SismoConnect(buildConfig(appId)) {}
+
     /**
      * @dev validates if the _op (UserOperation) matches the SessionKey permissions
      * and that _op has been signed by this SessionKey
@@ -19,16 +23,46 @@ contract VVASessionValidationModule {
         bytes calldata _sessionKeyData,
         bytes calldata _sessionKeySignature
     ) external view returns (bool) {
-        // public key of the user
+        // public key of the user [20 bytes]
         address sessionKey = address(bytes20(_sessionKeyData[0:20]));
 
-        // for now no validation is performed, we allow any calls for the existing session keys in the presense of the vlaid signature
+        // sismo groupId [16 bytes]
+        bytes16 groupId = bytes16(_sessionKeyData[20:36]);
 
-        // check that signature is corresponding to the public key
-        return
-            ECDSA.recover(
-                ECDSA.toEthSignedMessageHash(_userOpHash),
-                _sessionKeySignature
-            ) == sessionKey;
+        // everything we get from sismo proof of the user
+        bytes memory sismoConnectResponse = _sessionKeyData[36:]; // TODO check the slicing
+
+        SismoConnectResponse memory response = abi.decode(
+            sismoConnectResponse,
+            (SismoConnectResponse)
+        );
+        SismoConnectRequest memory request = buildRequest(
+            buildAuth({authType: AuthType.VAULT}),
+            buildClaim({groupId: groupId}),
+            buildSignature({message: abi.encode(sessionKey)})
+        ); // TODO correct to use sessionKey?
+
+        try _sismoConnectVerifier.verify(response, request, config()) returns (
+            SismoConnectVerifiedResult memory result
+        ) {
+            // if the proofs and signed message are valid, we can take the vaultId from the verified result
+            // it is the anonymous identifier of a user's vault for our VVA app
+            // --> vaultId = hash(userVaultSecret, appId)
+            uint256 vaultId = SismoConnectHelper.getUserId(
+                result,
+                AuthType.VAULT
+            );
+
+            // [OPTIONAL] TODO we can check for additional proofs?
+
+            // check that signature is corresponding to the public key
+            return
+                ECDSA.recover(
+                    ECDSA.toEthSignedMessageHash(_userOpHash),
+                    _sessionKeySignature
+                ) == sessionKey;
+        } catch {
+            return false;
+        }
     }
 }
