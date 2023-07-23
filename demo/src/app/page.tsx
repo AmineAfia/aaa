1c079c7ea9bf8bcbf8106aa3963397b6df60d7ac
iuwqyir <toomas.oosalu@gmail.com>
2023-07-23 03:20:21 +0200

    demo

---
+++ demo/src/app/page.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { useState } from "react";
+import Header from "./components/Header";
+import {
+  SismoConnectButton,
+  SismoConnectResponse,
+  SismoConnectVerifiedResult,
+} from "@sismo-core/sismo-connect-react";
+import {
+  CONFIG,
+  AUTHS,
+  CLAIMS,
+  SIGNATURE_REQUEST,
+  AuthType,
+  ClaimType,
+} from "./sismo-connect-config";
+import {ethers as hardhatEthers} from 'hardhat';
+
+import { makeEcdsaModuleUserOp } from '../../../test/utils/userOp'
+import {
+	getEcdsaOwnershipRegistryModule,
+	getEntryPoint,
+	getMockToken,
+	getSmartAccountFactory,
+	getSmartAccountImplementation,
+	getSmartAccountWithModule,
+	getVerifyingPaymaster,
+} from '../../../test/utils/setupHelper';
+const COMPANY_ADDRESS = '0xf3308Cfc92e0166D3811e09E2360A31D85312500'
+
+export default function Home() {
+  const [sismoConnectVerifiedResult, setSismoConnectVerifiedResult] =
+    useState<SismoConnectVerifiedResult>();
+  const [sismoConnectResponse, setSismoConnectResponse] = useState<SismoConnectResponse>();
+  const [pageState, setPageState] = useState<string>("init");
+  const [error, setError] = useState<string>("");
+
+  return (
+    <>
+      <main className="main">
+        <Header />
+        {pageState == "init" ? (
+          <>
+            <SismoConnectButton
+              config={CONFIG}
+              // Auths = Data Source Ownership Requests. (e.g Wallets, Github, Twitter, Github)
+              auths={AUTHS}
+              // Claims = prove group membership of a Data Source in a specific Data Group.
+              // (e.g ENS DAO Voter, Minter of specific NFT, etc.)
+              // Data Groups = [{[dataSource1]: value1}, {[dataSource1]: value1}, .. {[dataSource]: value}]
+              // Existing Data Groups and how to create one: https://factory.sismo.io/groups-explorer
+              claims={CLAIMS}
+              // Signature = user can sign a message embedded in their zk proof
+              signature={SIGNATURE_REQUEST}
+              text="Prove With Sismo"
+              // Triggered when received Sismo Connect response from user data vault
+              onResponse={async (response: SismoConnectResponse) => {
+                setSismoConnectResponse(response);
+                setPageState("verifying");
+                const proofAuth = response?.proofs?.find(proof => !!proof.auths?.length)?.auths?.find(auth => auth.authType === AuthType.EVM_ACCOUNT)
+                const actorAddress = proofAuth?.userId
+                console.log('user address', actorAddress)
+                console.log(JSON.stringify(response))
+
+                try {
+                  const [_, smartAccountOwner] = await hardhatEthers.getSigners();
+                  const ecdsaModule = await getEcdsaOwnershipRegistryModule();
+                  const entryPoint = await getEntryPoint();
+                  const ownerAddress = await smartAccountOwner.getAddress();
+                  const sessionKeyManager = await (await hardhatEthers.getContractFactory('SessionKeyManager')).deploy();
+                  let userOp = await makeEcdsaModuleUserOp(
+                    'enableModule',
+                    [sessionKeyManager.address],
+                    COMPANY_ADDRESS,
+                    ownerAddress,
+                    entryPoint,
+                    ecdsaModule.address
+                  );
+              
+                  await new Promise((f) => setTimeout(f, 10000));
+                  await entryPoint.handleOps([userOp], actorAddress);
+                } catch (err) {
+                  console.log('error setting up smart account... ', err)
+                }
+
+                /* if (verifiedResult.ok) {
+                  setSismoConnectVerifiedResult(data);
+                  setPageState("verified");
+                } else {
+                  setPageState("error");
+                  setError(data);
+                } */
+              }}
+            />
+          </>
+        ) : (
+          <>
+            <button
+              onClick={() => {
+                window.location.href = "/";
+              }}
+            >
+              {" "}
+              RESET{" "}
+            </button>
+            <br></br>
+            <div className="status-wrapper">
+              {pageState == "verifying" ? (
+                <span className="verifying"> Verifying ZK Proofs... </span>
+              ) : (
+                <>
+                  {Boolean(error) ? (
+                    <span className="error"> Error verifying ZK Proofs: {error} </span>
+                  ) : (
+                    <span className="verified"> ZK Proofs verified!</span>
+                  )}
+                </>
+              )}
+            </div>
+          </>
+        )}
+      </main>
+    </>
+  );
+}
